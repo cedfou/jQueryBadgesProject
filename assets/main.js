7a9abc5e7c5d3e25e942cda3805a8ebffb9c5d13
Cédric Fourneau <cedfou@gmail.com>
2017-01-14 23:28:53 +0100

    retrieve CodeSchool badges in AJAX
    first try

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/cedfou.json',
+    dataType: 'jsonp',
+    success: function(response) {
+console.log(response.courses.completed);
+    var badgesElements = $.map(response.courses.completed, function(badge, i) {
+       var listItem = $('<div class="course"></div>');
+       $('<h3>'+badge.title+'</h3>').appendTo(listItem);
+       $('<img src="'+badge.badge+'" alt="'+badge.title+'" />').appendTo(listItem);
+      $('<a href="'+badge.url+'" alt="'+badge.title+'" target="_blank" class="btn btn-primary">See course</a>').appendTo(listItem);
+       return listItem;
+});
+     $('#badges').html(badgesElements);
+      //$("#badges").text(JSON.stringify(response));
+    }
+  });
 
 });
